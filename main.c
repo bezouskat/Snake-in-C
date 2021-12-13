8fdc487da4342c29a8a7056a15cbab9d791e2a4c
bezouskat <bezouska.t@gmail.com>
2021-12-13 21:18:31 +0100

    final version
    settings option added

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "settings.h"
+#include "state_play.h"
+#include "state_menu.h"
+#include "state_settings.h"
+
+Settings settings;
 
 int main() {
-    printf("Hello, World!\n");
+    set_window_title("My Own Snake Game - Bezouska. T");
+    set_window_size(609, 438);
+    set_cursor_visible(FALSE);
+
+    settings.start_length = 1;
+    settings.start_level = 1;
+    settings.running = true;
+    settings.current_state = MENU;
+
+    srand(time(NULL));
+
+    while (settings.running)
+    {
+        switch (settings.current_state)
+        {
+            case MENU: state_menu(); break;
+            case PLAY: state_play(); break;
+            case SETTINGS: state_settings(); break;
+        }
+    }
+
     return 0;
 }
